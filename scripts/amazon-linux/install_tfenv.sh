597c83a9667e07ba7dc757c28661c4a87b56dfb8
Dwi Fahni Denni <dfdenni0912@gmail.com>
2022-03-06 13:36:11 +0700

    Fixing symlink installation userdata

--- scripts/amazon-linux/install_tfenv.sh
+++ scripts/amazon-linux/install_tfenv.sh
@@ -5,9 +5,9 @@
 # ================================================================================================
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >>~/.bash_profile
-ln -s ~/.tfenv/bin/* /usr/local/bin
+ln -sf ~/.tfenv/bin/* /usr/local/bin
 
 mkdir -p ~/.local/bin/
 . ~/.profile
-ln -s ~/.tfenv/bin/* ~/.local/bin
+ln -sf ~/.tfenv/bin/* ~/.local/bin
 which tfenv
--- scripts/ubuntu/install_tfenv.sh
+++ scripts/ubuntu/install_tfenv.sh
@@ -7,9 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >>~/.bash_profile
-ln -s ~/.tfenv/bin/* /usr/local/bin
+ln -sf ~/.tfenv/bin/* /usr/local/bin
 
 mkdir -p ~/.local/bin/
 . ~/.profile
-ln -s ~/.tfenv/bin/* ~/.local/bin
+ln -sf ~/.tfenv/bin/* ~/.local/bin
 which tfenv
