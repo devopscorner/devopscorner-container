7cb961f1fec2b0c5c020ae1fda5cef6f76d42589
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-03-10 18:20:21 +0700

    Refactoring container cicd-codebuild

--- compose/docker/cicd-codebuild/start-build.sh
+++ compose/docker/cicd-codebuild/start-build.sh
@@ -25,21 +25,20 @@ echo ""
 TAG="codebuild-4.0"
 echo " Build Image => $IMAGE:$TAG"
 docker build --no-cache -f Dockerfile-CodeBuild-4.0 -t $IMAGE:$TAG .
-echo ""
-
-TAG="codebuild-5.0"
-echo " Build Image => $IMAGE:$TAG"
-docker build --no-cache -f Dockerfile-CodeBuild-5.0 -t $IMAGE:$TAG .
 docker tag $IMAGE:$TAG $IMAGE:codebuild
 docker tag $IMAGE:$TAG $IMAGE:codebuild-latest
 docker tag $IMAGE:$TAG $IMAGE:1.23-latest
 docker tag $IMAGE:$TAG $IMAGE:latest
 echo ""
 
-echo "Cleanup Unknown Tags"
-echo "docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi"
-docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi
-echo ""
+# TAG="codebuild-5.0"
+# echo " Build Image => $IMAGE:$TAG"
+# docker build --no-cache -f Dockerfile-CodeBuild-5.0 -t $IMAGE:$TAG .
+# docker tag $IMAGE:$TAG $IMAGE:codebuild
+# docker tag $IMAGE:$TAG $IMAGE:codebuild-latest
+# docker tag $IMAGE:$TAG $IMAGE:1.23-latest
+# docker tag $IMAGE:$TAG $IMAGE:latest
+# echo ""
 
 echo "Cleanup Unknown Tags"
 echo "docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi"
