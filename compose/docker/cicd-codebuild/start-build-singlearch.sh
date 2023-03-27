4a181c80cd8853d3e10c2cc45ae09fa796b30c1a
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-03-28 06:47:40 +0700

    Update codebuild container builder

--- compose/docker/cicd-codebuild/start-build-singlearch.sh
+++ compose/docker/cicd-codebuild/start-build-singlearch.sh
@@ -12,12 +12,15 @@ export CI_PROJECT_NAME="cicd"
 
 export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
 
+## CodeBuild can't be use for Multi Arch ##
 ## Using Public ECR:
-## public.ecr.aws/codebuild/local-builds:latest
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:2.0
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:3.0
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:4.0
 
-export PLATFORM="linux/amd64,linux/arm64"
+export PLATFORM="linux/amd64"
 
-export STACKS_NAME="devopscorner-multiarch"
+export STACKS_NAME="devopscorner-singlearch"
 # List PLATFORM:
 # docker buildx inspect $STACKS_NAME
 
@@ -50,10 +53,42 @@ use_stack() {
     echo ''
 }
 
-build_codebuild_localbuild_latest() {
-    TAGS="codebuild-localbuild \
-        codebuild-localbuild-latest \
-        1.23-codebuild-localbuild "
+build_codebuild_20() {
+    TAG="codebuild-2.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $PLATFORM \
+        -f Dockerfile-CodeBuild-2.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_30() {
+    TAG="codebuild-3.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $PLATFORM \
+        -f Dockerfile-CodeBuild-3.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_40() {
+    TAG="codebuild-4.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $PLATFORM \
+        -f Dockerfile-CodeBuild-4.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_latest() {
+    TAGS="codebuild \
+        codebuild-nginx-1.23 \
+        codebuild-latest \
+        1.23-codebuild \
+        latest "
 
     for TAG in $TAGS; do
         echo " Build Image => $IMAGE:$TAG"
@@ -66,7 +101,10 @@ build_codebuild_localbuild_latest() {
 }
 
 docker_build() {
-    build_codebuild_localbuild_latest
+    build_codebuild_20
+    build_codebuild_30
+    build_codebuild_40
+    build_codebuild_latest
 }
 
 docker_clean() {
