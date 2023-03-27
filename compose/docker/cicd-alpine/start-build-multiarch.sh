37d685ab70406d781e33c8fecf308b434c243aa2
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-03-27 15:14:18 +0700

    Refactoring build container script

--- compose/docker/cicd-alpine/start-build-multiarch.sh
+++ compose/docker/cicd-alpine/start-build-multiarch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # -----------------------------------------------------------------------------
-#  Docker Build Container
+#  Docker Build Multi Architecture Container
 # -----------------------------------------------------------------------------
 #  Author     : Dwi Fahni Denni
 #  License    : Apache v2
@@ -11,17 +11,49 @@ export CI_PROJECT_PATH="devopscorner"
 export CI_PROJECT_NAME="cicd"
 
 export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
+export PLATFORM="linux/amd64,linux/arm64"
+
+export STACKS_NAME="devopscorner-multiarch"
+# List PLATFORM:
+# docker buildx inspect $STACKS_NAME
+
+line1="----------------------------------------------------------------------------------------------------"
+line2="===================================================================================================="
+
+create_stack() {
+    echo $line2
+    echo " Build Stacks Multiplatform"
+    echo " Stacks: $STACKS_NAME"
+    echo $line2
+    echo " -> docker buildx create --name $STACKS_NAME --driver docker-container --bootstrap"
+    echo $line1
+    docker buildx create --name $STACKS_NAME --driver docker-container --bootstrap
+    echo " - DONE -"
+    echo ""
+}
+
+use_stack() {
+    echo $line2
+    echo " Use Stacks Multiplatform"
+    echo " Stacks: $STACKS_NAME"
+    echo $line2
+    echo " -> docker buildx use $STACKS_NAME"
+    echo $line1
+    docker buildx use $STACKS_NAME
+    echo " - DONE -"
+    echo ""
+}
 
 build_alpine_315() {
     # DEPRECIATED for Alpine-3.15 ##
     TAG="alpine-3.15-AnsibleTower"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.15-Ansible-Tower -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.15-Ansible-Tower -t $IMAGE:$TAG .
     echo ""
 
     TAG="alpine-3.15-AWX"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.15-AWX -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.15-AWX -t $IMAGE:$TAG .
     docker tag $IMAGE:$TAG $IMAGE:alpine-3.15
     echo ""
 }
@@ -29,12 +61,12 @@ build_alpine_315() {
 build_alpine_316() {
     TAG="alpine-3.16-AnsibleTower"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.16-Ansible-Tower -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.16-Ansible-Tower -t $IMAGE:$TAG .
     echo ""
 
     TAG="alpine-3.16-AWX"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.16-AWX -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.16-AWX -t $IMAGE:$TAG .
     docker tag $IMAGE:$TAG $IMAGE:alpine-3.16
     echo ""
 }
@@ -42,19 +74,19 @@ build_alpine_316() {
 build_alpine_317() {
     TAG="alpine-3.17-AnsibleTower"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.17-Ansible-Tower -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.17-Ansible-Tower -t $IMAGE:$TAG .
     echo ""
 
     TAG="alpine-3.17-AWX"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile-Alpine-3.17-AWX -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile-Alpine-3.17-AWX -t $IMAGE:$TAG .
     echo ""
 }
 
 build_alpine_latest() {
     TAG="alpine-nginx-1.23"
     echo " Build Image => $IMAGE:$TAG"
-    docker build --no-cache -f Dockerfile -t $IMAGE:$TAG .
+    docker buildx build --platform $PLATFORM --no-cache -f Dockerfile -t $IMAGE:$TAG .
     docker tag $IMAGE:$TAG $IMAGE:alpine
     docker tag $IMAGE:$TAG $IMAGE:alpine-latest
     docker tag $IMAGE:$TAG $IMAGE:1.23-alpine
@@ -63,7 +95,8 @@ build_alpine_latest() {
 }
 
 docker_build() {
-    #build_alpine_315
+    # Depreciated Alpine-3.15
+    # build_alpine_315
     build_alpine_316
     build_alpine_317
     build_alpine_latest
@@ -77,6 +110,8 @@ docker_clean() {
 }
 
 main() {
+  create_stack
+  use_stack
   docker_build
   docker_clean
   echo ''
