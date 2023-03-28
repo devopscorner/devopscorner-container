1a8582562479dd485fb781f14259dfc1f902f2de
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-03-28 15:13:57 +0700

    Refactoring cicd container for multi platform (multi architecture) linux/amd64 and linux/arm64

---
+++ compose/docker/cicd-alpine/start-build-arm64.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env sh
+# -----------------------------------------------------------------------------
+#  Docker Build Container (Linux/AMD64)
+# -----------------------------------------------------------------------------
+#  Author     : Dwi Fahni Denni
+#  License    : Apache v2
+# -----------------------------------------------------------------------------
+set -e
+
+export CI_PROJECT_PATH="devopscorner"
+export CI_PROJECT_NAME="cicd"
+
+export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
+
+export TARGETPLATFORM="linux/arm64"
+export LINUX_ARCH_PATH="arch-arm64"
+
+build_alpine_315() {
+    # DEPRECIATED for Alpine-3.15 ##
+    TAG="alpine-3.15-AnsibleTower"
+    echo " Build Image => $IMAGE:$TAG"
+    docker build \
+        -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.15-Ansible-Tower \
+        -t $IMAGE:$TAG .
+    echo ''
+
+    TAGS="alpine-3.15 \
+        alpine-3.15-AWX "
+    for TAG in $TAGS; do
+        echo " Build Image => $IMAGE:$TAG"
+        docker build \
+            -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.15-AWX \
+            -t $IMAGE:$TAG .
+        echo ''
+    done
+}
+
+build_alpine_316() {
+    TAG="alpine-3.16-AnsibleTower"
+    echo " Build Image => $IMAGE:$TAG"
+    docker build \
+        -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.16-Ansible-Tower \
+        -t $IMAGE:$TAG .
+    echo ''
+
+    TAGS="alpine-3.16 \
+        alpine-3.16-AWX "
+    for TAG in $TAGS; do
+        echo " Build Image => $IMAGE:$TAG"
+        docker build \
+            -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.16-AWX \
+            -t $IMAGE:$TAG .
+        echo ''
+    done
+}
+
+build_alpine_317() {
+    TAG="alpine-3.17-AnsibleTower"
+    echo " Build Image => $IMAGE:$TAG"
+    docker build \
+        -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.17-Ansible-Tower \
+        -t $IMAGE:$TAG .
+    echo ''
+
+    TAGS="alpine-3.17 \
+        alpine-3.17-AWX "
+    for TAG in $TAGS; do
+        echo " Build Image => $IMAGE:$TAG"
+        docker build \
+            -f $LINUX_ARCH_PATH/Dockerfile-Alpine-3.17-AWX \
+            -t $IMAGE:$TAG .
+        echo ''
+    done
+}
+
+build_alpine_latest() {
+    TAGS="alpine \
+        alpine-nginx-1.23 \
+        alpine-latest \
+        1.23-alpine "
+
+    for TAG in $TAGS; do
+        echo " Build Image => $IMAGE:$TAG"
+        docker build \
+            -f $LINUX_ARCH_PATH/Dockerfile \
+            -t $IMAGE:$TAG .
+        echo ''
+    done
+}
+
+docker_build() {
+    # Depreciated Alpine-3.15
+    # build_alpine_315
+    build_alpine_316
+    build_alpine_317
+    build_alpine_latest
+}
+
+docker_clean() {
+    echo "Cleanup Unknown Tags"
+    echo "docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi"
+    docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi
+    echo ''
+}
+
+main() {
+    docker_build
+    docker_clean
+    echo ''
+    echo '-- ALL DONE --'
+}
+
+### START HERE ###
+main
---
+++ compose/docker/cicd-codebuild/start-buildx-amd64-codebuild.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env sh
+# -----------------------------------------------------------------------------
+#  Docker Build Multi Architecture Container
+# -----------------------------------------------------------------------------
+#  Author     : Dwi Fahni Denni
+#  License    : Apache v2
+# -----------------------------------------------------------------------------
+set -e
+
+export CI_PROJECT_PATH="devopscorner"
+export CI_PROJECT_NAME="cicd"
+
+export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
+
+## CodeBuild can't be use for Multi Arch ##
+## Using Public ECR:
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:2.0
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:3.0
+## public.ecr.aws/codebuild/amazonlinux2-x86_64-standard:4.0
+
+export TARGETPLATFORM="linux/amd64"
+export LINUX_ARCH_PATH="arch-amd64"
+export STACKS_NAME="devopscorner-amd64"
+# List PLATFORM:
+# docker buildx inspect $STACKS_NAME
+
+line1="----------------------------------------------------------------------------------------------------"
+line2="===================================================================================================="
+
+create_stack() {
+    echo $line2
+    echo " Build Stacks Multiplatform"
+    echo " Stacks: $STACKS_NAME"
+    echo $line2
+    echo " -> docker buildx create --name $STACKS_NAME --driver docker-container --bootstrap"
+    echo $line1
+    docker buildx create --name $STACKS_NAME \
+        --driver docker-container \
+        --bootstrap
+    echo " - DONE -"
+    echo ''
+}
+
+use_stack() {
+    echo $line2
+    echo " Use Stacks Multiplatform"
+    echo " Stacks: $STACKS_NAME"
+    echo $line2
+    echo " -> docker buildx use $STACKS_NAME"
+    echo $line1
+    docker buildx use $STACKS_NAME
+    echo " - DONE -"
+    echo ''
+}
+
+build_codebuild_20() {
+    TAG="codebuild-2.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $TARGETPLATFORM \
+        -f $LINUX_ARCH_PATH/Dockerfile-CodeBuild-2.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_30() {
+    TAG="codebuild-3.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $TARGETPLATFORM \
+        -f $LINUX_ARCH_PATH/Dockerfile-CodeBuild-3.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_40() {
+    TAG="codebuild-4.0"
+    echo " Build Image => $IMAGE:$TAG"
+    docker buildx build --push \
+        --platform $TARGETPLATFORM \
+        -f $LINUX_ARCH_PATH/Dockerfile-CodeBuild-4.0 \
+        -t $IMAGE:$TAG .
+    echo ''
+}
+
+build_codebuild_latest() {
+    TAGS="codebuild \
+        codebuild-nginx-1.23 \
+        codebuild-latest \
+        1.23-codebuild \
+        latest "
+
+    for TAG in $TAGS; do
+        echo " Build Image => $IMAGE:$TAG"
+        docker buildx build --push \
+            --platform $TARGETPLATFORM \
+            -f $LINUX_ARCH_PATH/Dockerfile \
+            -t $IMAGE:$TAG .
+        echo ''
+    done
+}
+
+docker_build() {
+    build_codebuild_20
+    build_codebuild_30
+    build_codebuild_40
+    build_codebuild_latest
+}
+
+docker_clean() {
+    echo "Cleanup Unknown Tags"
+    echo "docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi"
+    docker images -a | grep none | awk '{ print $3; }' | xargs docker rmi
+    echo ''
+}
+
+main() {
+    # create_stack
+    use_stack
+    docker_build
+    docker_clean
+    echo ''
+    echo '-- ALL DONE --'
+}
+
+### START HERE ###
+main
